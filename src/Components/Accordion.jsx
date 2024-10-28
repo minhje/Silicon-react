e87f6eb58674986d3356b40a5101d7fe7ba02dda
minhje <mminnahjelm@gmail.com>
2024-10-28 15:11:29 +0100

    Ny accordion

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,145 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Accordion = () => {
+  // Använder useState för att hålla koll på vilken fråga som är öppen
+  const [openIndex, setOpenIndex] = useState(null);
+
+  // Exempeldata för FAQ
+  const faqData = [
+    {
+      question: "Lagrar appen någon personlig information?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia iste cumque repellat est quasi aperiam ratione aut rem aliquid. Laborum!",
+    },
+    {
+      question: "Hur säker är min data i appen?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus et expedita est, asperiores dolor ab iste provident earum recusandae nam.",
+    },
+    {
+      question: "Vad händer om jag tar bort mitt konto?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia iste cumque repellat est quasi aperiam ratione aut rem aliquid. Laborum!",
+    },
+    {
+        question: "Lagrar appen någon personlig information?",
+        answer:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia iste cumque repellat est quasi aperiam ratione aut rem aliquid. Laborum!",
+      },
+      {
+        question: "Hur säker är min data i appen?",
+        answer:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus et expedita est, asperiores dolor ab iste provident earum recusandae nam.",
+      },
+      {
+        question: "Vad händer om jag tar bort mitt konto?",
+        answer:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia iste cumque repellat est quasi aperiam ratione aut rem aliquid. Laborum!",
+      },
+  ];
+
+  // Funktion för att öppna och stänga FAQ-element
+  const handleToggle = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
   return (
-    <>
-     {/* Grunden till denna del är generarad av AI, där jag senare redigerat innehållet */}
-     <div className="accordion"> 
-
-     {/* Accordion Item 1 */}
-     <div className="accordion-item">
-         <input type="checkbox" id="item1"/>
-         <label htmlFor="item1" className="accordion-title">Is any of my personal information
-             stored in the App?  
-             <span className="btn-circle-down">
-                 <i className="fa-solid fa-chevron-down"></i>
-             </span>
-             <span className="btn-circle-up">
-                 <i className="fa-solid fa-chevron-up"></i>
-             </span>
-         </label>
-         <div className="accordion-content">
-             <p>Nunc duis id aenean gravida tincidunt eu, tempor
-                 ullamcorper. Viverra aliquam arcu, viverra et,
-                 cursus. Aliquet pretium cursus adipiscing gravida
-                 et consequat lobortis arcu velit. Nibh pharetra
-                 fermentum duis accumsan lectus non. Massa
-                 cursus molestie lorem scelerisque pellentesque.
-                 Nisi, enim, arcu purus gravida adipiscing euismod
-                 montes, duis egestas. Vehicula eu etiam quam
-                 tristique tincidunt suspendisse ut consequat.</p>
-                 <p>Ornare senectus fusce dignissim ut. Integer
-                     consequat in eu tortor, faucibus et lacinia
-                     posuere. Turpis sit viverra lorem suspendisse
-                     lacus aliquam auctor vulputate. Quis egestas
-                     aliquam nunc purus lacus, elit leo elit facilisi.
-                     Dignissim amet adipiscing massa integer.</p>
-         </div>
-     </div>
- 
-     { /* Accordion Item 2 */ }
-     <div className="accordion-item">
-         <input type="checkbox" id="item2"/>
-         <label htmlFor="item2" className="accordion-title">What formats can I download my
-             transaction history in?
-             <span className="btn-circle-down">
-                 <i className="fa-solid fa-chevron-down"></i>
-             </span>
-             <span className="btn-circle-up">
-                 <i className="fa-solid fa-chevron-up"></i>
-             </span>
-         </label>
-         <div className="accordion-content">
-             <p>Nunc duis id aenean gravida tincidunt eu, tempor
-                 ullamcorper. Viverra aliquam arcu, viverra et,
-                 cursus. Aliquet pretium cursus adipiscing gravida
-                 et consequat lobortis arcu velit. Nibh pharetra
-                 fermentum duis accumsan lectus non. Massa
-                 cursus molestie lorem scelerisque pellentesque.</p>
-         </div>
-     </div>
- 
-      {/* Accordion Item 3  */}
-     <div className="accordion-item">
-         <input type="checkbox" id="item3"/>
-         <label htmlFor="item3" className="accordion-title">Can I schedule future transfers?
-             <span className="btn-circle-down">
-                 <i className="fa-solid fa-chevron-down"></i>
-             </span>
-             <span className="btn-circle-up">
-                 <i className="fa-solid fa-chevron-up"></i>
-             </span>
-         </label>
-         <div className="accordion-content">
-             <p>Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. 
-                 Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. 
-                 Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. 
-                 Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat.</p>
-         </div>
-     </div>
-
-     {/* Accordion Item 4 */}
-     <div className="accordion-item">
-         <input type="checkbox" id="item4"/>
-         <label htmlFor="item4" className="accordion-title">When can I use Banking App
-             services?
-             <span className="btn-circle-down">
-                 <i className="fa-solid fa-chevron-down"></i>
-             </span>
-             <span className="btn-circle-up">
-                 <i className="fa-solid fa-chevron-up"></i>
-             </span>
-         </label>
-         <div className="accordion-content">
-             <p>Nunc duis id aenean gravida tincidunt eu, tempor
-                 ullamcorper. Viverra aliquam arcu, viverra et,
-                 cursus. Aliquet pretium cursus adipiscing gravida
-                 et consequat lobortis arcu velit. Nibh pharetra
-                 fermentum duis accumsan lectus non. Massa
-                 cursus molestie lorem scelerisque pellentesque.
-                 Nisi, enim, arcu purus gravida adipiscing euismod
-                 montes, duis egestas.</p>
-         </div>
-     </div>
- 
-      {/* Accordion Item 5  */}
-     <div className="accordion-item">
-         <input type="checkbox" id="item5"/>
-         <label htmlFor="item5" className="accordion-title">Can I create my own password that is
-             easy for me to remember?
-             <span className="btn-circle-down">
-                 <i className="fa-solid fa-chevron-down"></i></span>
-                 <span className="btn-circle-up">
-                     <i className="fa-solid fa-chevron-up"></i>          
-             </span></label>
-         <div className="accordion-content">
-             <p>Nunc duis id aenean gravida tincidunt eu, tempor
-                 ullamcorper. Viverra aliquam arcu, viverra et,
-                 cursus.</p>
-         </div>
-     </div>
- 
-      {/* Accordion Item 6 */}
-        <div className="accordion-item">
-         <input type="checkbox" id="item6"/>
-         <label htmlFor="item6" className="accordion-title">What happens if I forget or lose my
-             password?
-             <span className="btn-circle-down">
-                 <i className="fa-solid fa-chevron-down"></i>
-             </span>
-             <span className="btn-circle-up">
-                 <i className="fa-solid fa-chevron-up"></i>
-             </span></label>
-            <div className="accordion-content">
-             <p>Nunc duis id aenean gravida tincidunt eu, tempor
-                 ullamcorper. Viverra aliquam arcu, viverra et,
-                 cursus. Aliquet pretium cursus adipiscing gravida
-                 et consequat lobortis arcu velit.</p>
+    <div className="accordion">
+      {faqData.map((item, index) => (
+        <div key={index} className={`faq-card ${openIndex === index ? 'open' : ''}`}>
+          <div className="question" onClick={() => handleToggle(index)}>
+            <p>{item.question}</p>
+            <button className="btn-round">
+              <i className={`fa-solid fa-chevron-${openIndex === index ? 'up' : 'down'}`}></i>
+            </button>
+          </div>
+          {openIndex === index && (
+            <div className="answer">
+              <div className="expandable">{item.answer}</div>
             </div>
+          )}
         </div>
+      ))}
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Accordion
+export default Accordion;
--- src/Components/DarkmodeSwitch.jsx
+++ src/Components/DarkmodeSwitch.jsx
@@ -1,47 +1,4 @@
-// import React, { useEffect, useState } from 'react'
-
-// const DarkmodeSwitch = () => {
-//     const [isDarkmode, setIsDarkmode] = useState(false);
-
-//     const toggleDarkmode = () => {
-//         const newMode = !isDarkmode;
-//         setIsDarkmode(newMode);
-      
-//         if (newMode) {
-//           document.documentElement.setAttribute('data-theme', 'dark');
-//           localStorage.setItem('theme', 'dark');
-//         }
-//         else {
-//           document.documentElement.setAttribute('data-theme', 'light');
-//           localStorage.setItem('theme', 'light');
-//         }
-//     }
-
-//     useEffect(() => {
-//         const savedTheme = localStorage.getItem('theme')
-//         if (savedTheme === 'dark') {
-//             document.documentElement.setAttribute('data-theme', 'dark');
-//             setIsDarkmode(true)
-//           }
-//           else {
-//             document.documentElement.setAttribute('data-theme', 'light');
-//             setIsDarkmode(false)
-//           }
-//     }, [])
-
-//   return (
-//     <div className="btn-toggle-switch">
-//     <span className="label">Dark Mode</span>
-
-//     <label htmlFor="darkmode-switch" className="toggle-switch">
-//         <input type="checkbox" checked={isDarkmode} onChange={toggleDarkmode} />
-//         <span className="slider round"></span>
-//     </label>
-//  </div>
-//   )
-// }
-
-// export default DarkmodeSwitch
+// Code from Hans video "Omvandla din befintliga JavaScript-kod till React", adjusted to fit my previous code. 
 
 import React, { useEffect, useState } from 'react'
 
@@ -76,7 +33,7 @@ const DarkmodeSwitch = () => {
 
   return (
     <div id="darkmode-toggle-switch" className="btn-toggle-switch">
-    <span className="label">Dark Mode</span>
+    <span className="label"> Dark Mode</span>
 
     <label htmlFor="darkmode-switch" className="toggle-switch">
 
