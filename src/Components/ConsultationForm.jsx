a566572d1fcaab5a55a7ec83cb7ac1e4b9c54611
minhje <mminnahjelm@gmail.com>
2024-11-06 14:01:02 +0100

    contact sida klar

--- src/Components/ConsultationForm.jsx
+++ src/Components/ConsultationForm.jsx
@@ -1,27 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const ConsultationForm = () => {
+  const [options, setOptions] = useState([{ id: 1, text: "Specialist 1" }, { id: 2, text: "Specialist 2" }]) 
+  const [formData, setFormData] = useState({fullName: '', email: '', specialist: options[0].id });
+  const [errors, setErrors] = useState({});
+  const [submitted, setSubmitted] = useState(false);
+
+
+  const validateField = (name, value) => {
+    let error = '';
+
+    if (name === 'fullName' && !/^[A-Öa-ö\s\-]{2,}$/.test(value)) {
+      error = "Your name must be at least 2 characters and can't contain numbers";
+    } else if (name === 'email' && !/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(value)) {
+      error = "Please enter a valid email address (example@domain.com)";
+    }
+
+    setErrors(prevErrors => ({ ...prevErrors, [name]: error }));
+  };
+
+  const validateForm = () => {
+    const newErrors = {}
+
+    if (!/^[A-Öa-ö\s\-]{2,}$/.test(formData.fullName)) {
+      newErrors.fullName = "Your name must be at least 2 characters and can't contain numbers";
+    }
+
+    if (!/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(formData.email)) {
+      newErrors.email = "Please enter a valid email address (example@domain.com)";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    validateField(name, value);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (validateForm()) {
+        const res = await fetch(
+          'https://win24-assignment.azurewebsites.net/api/forms/contact',
+          {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(formData),
+          })
+
+    
+    if (res.ok) {
+      setSubmitted(true)
+      setFormData({ fullName:'', email: '', specialist:'' })
+
+  }
+}
+}
+
+const handleOK = () => {
+  setSubmitted(false)
+}
+
   return (
     <div className="form-container">
-                    <h3>Get Online Consultation</h3>
-                    <form id='consult-form'>
-                        <label htmlFor="fullName" className="form-label">Full Name</label>
-                        <input type="text" id='fullName' />  
+      {submitted ? (
+        <div className="thank-you">
+          <h3>Thank you for making an appointment!</h3>
+          <p>We'll contact you as soon as possible</p>
+          <button className='btn-primary' onClick={handleOK}>OK</button>
+        </div>
+      ) : (
+        <>
+          <h3>Get Online Consultation</h3>
+          <form id="consult-form" onSubmit={handleSubmit} noValidate>
+            <label htmlFor="fullName" className="form-label">Full Name</label>
+            <input
+              type="text"
+              id="fullName"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleInputChange}
+              required
+            />
+            {errors.fullName && <span className="validation-error">{errors.fullName}</span>}
 
-                        <label htmlFor="email" className="form-label">Email adress</label>
-                        <input type="email" id="email" className="form-input" required></input>
-                        
-                        <label htmlFor="specialist" className="form-label">Specialist</label>
-                        <select id="specialist" name='specialist'>
-                            <option value=""></option>
-                            <option value="Specialist 2">Specialist 1</option>
-                            <option value="Specialist 2">Specialist 1</option>
-                        </select>
-                        
-                        <button type='submit' className='btn-primary'>Make an appointment</button>   
-                    </form>
-                </div>
+            <label htmlFor="email" className="form-label">Email address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="form-input"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+            {errors.email && <span className="validation-error">{errors.email}</span>}
+
+            <label htmlFor="options" className="form-label">Specialist</label>
+            <select
+              id="specialist"
+              name="specialist"
+              value={formData.specialist}
+              onChange={handleInputChange}
+              required
+            >
+              {options.map(option => (
+                <option key={option.id} value={option.id}>
+                  {option.text}
+                </option>
+              ))}
+            </select>
+
+            <button type="submit" className="btn-primary">Make an appointment</button>
+          </form>
+          </>
+      )}
+    </div>
   )
 }
 
-export default ConsultationForm
+export default ConsultationForm;
--- src/Components/SubscribeForm.jsx
+++ src/Components/SubscribeForm.jsx
@@ -23,7 +23,7 @@ const SubscribeForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const emailRegex = /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
         const newErrors = {}
         
         Object.keys(formData).forEach(field => {
